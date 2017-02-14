e89b234c581b9bc6acfac3cd9c6c5b9d288cad7d
vatbub <vatbub123+github@googlemail.com>
2017-02-14 02:55:29 +0100

    [ci skip] Added license headers to all classes

--- src/main/java/mslinks/data/GUID.java
+++ src/main/java/mslinks/data/GUID.java
@@ -14,14 +14,34 @@
 */
 package mslinks.data;
 
+/*-
+ * #%L
+ * FOKProjects MSLinks
+ * %%
+ * Copyright (C) 2016 - 2017 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import io.ByteReader;
 import io.ByteWriter;
 import io.Bytes;
+import mslinks.Serializable;
 
 import java.io.IOException;
 import java.util.Random;
-
-import mslinks.Serializable;
 
 public class GUID implements Serializable {
 	private static Random r = new Random();
